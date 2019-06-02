f312f5f628257f15b6d0f11ecb3fdd60fb05e1a5
Your Name <blmcilingir@hotmail.com>
2019-06-02 12:51:56 +0300

    changes

---
+++ src/test/java/com/test/automation/JenkinsTestAutomation/SuiteTest.java
@@ -0,0 +1,12 @@
+package com.test.automation.JenkinsTestAutomation;
+
+import org.testng.annotations.Test;
+
+public class SuiteTest {
+	
+	@Test
+	public void mockTest() {
+		System.out.println("Hello World");
+	}
+
+}
