5f6de8a05290424baf477a3b16e30d70cfd9b4a0
Your Name <blmcilingir@hotmail.com>
2019-06-02 14:04:37 +0300

    hello

--- src/main/java/com/test/automation/JenkinsTestAutomation/App.java
+++ src/main/java/com/test/automation/JenkinsTestAutomation/App.java
@@ -1,5 +1,9 @@
 package com.test.automation.JenkinsTestAutomation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 /**
@@ -8,7 +12,27 @@ import org.testng.annotations.Test;
  */
 public class App {
 	@Test
-	public void main() {
-		System.out.println("Hello World!");
+	public void main() throws InterruptedException {
+
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:/Users/Toshiba/Downloads/chromedriver_74.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.koton.com/tr/");
+		//Assert.assertTrue(driver.getTitle().contains("koton")); // doğrulama 
+		driver.manage().window().maximize();
+		
+		WebElement searchElement = driver.findElement(By.name("text"));//arama butonunu buluyoruz
+		searchElement.sendKeys("bluz"); 
+		searchElement.submit();
+		Thread.sleep(9000);
+		
+		WebElement xsElement = driver.findElement(By.xpath(".//div[@class='productListFilter']//div[@class='filterWrapper'][4]//li[@class='show']//label[@for='XS']"));
+				
+		if (!xsElement.isSelected()) {
+			xsElement.click();
+		}
+
+		driver.quit();
+		
 	}
 }
