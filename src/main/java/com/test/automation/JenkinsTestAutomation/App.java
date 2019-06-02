a620048ed915d36b53270fbf8b863e7744d41341
Your Name <blmcilingir@hotmail.com>
2019-06-02 13:41:37 +0300

    hello

--- src/main/java/com/test/automation/JenkinsTestAutomation/App.java
+++ src/main/java/com/test/automation/JenkinsTestAutomation/App.java
@@ -6,11 +6,9 @@ import org.testng.annotations.Test;
  * Hello world!
  *
  */
-public class App 
-{	
+public class App {
 	@Test
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public void main() {
+		System.out.println("Hello World!");
+	}
 }
